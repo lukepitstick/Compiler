d2008d3e95656a2ad6df87ae5f5aded491e6bcba
Kwangju Kim <kimk3@miamioh.edu>
2016-03-09 12:26:47 -0500

    sample code given

--- proj5/codegenerator.py
+++ proj5/codegenerator.py
@@ -4,31 +4,79 @@ import treetraverse
 
 toWrite = [] # initialize what to write
 
+# After the code generation has been done
+# We will write everything inside 'toWrite' into
+# output.asm file.
+
 def PushCodes(InstToken):
     pass
 
+# Generally does nothing, but we can make
+# our own mean of function to do smth
 def START():
     pass
 
+# If reaching this function,
+# We have to invoke the code generation.
 def FINISH():
+"""
+Exit syscall is 10
+
+Ex)
+li $??, 10
+syscall
+"""
     pass
 
 # Syscall the KeyboardInput (Equivalent to Java's Scanner.next())
 def READ_IDS():
+"""
+Here, we are receiving data so...
+(Maybe integer value?)
+Syscall integers:
+5 - read integer
+8 - read string
+At this point we are not likely to use 6, 7 which is
+read float and double respectively.
+
+Ex)
+li $v0, 5;
+syscall
+
+Loads whatsoever value you type to $vx register
+"""
     pass
 
 # Syscall the Print (Equivalent to Java's System.out.println())
 def WRITE_IDS():
+"""
+Here, we are printing data to the CMD
+
+Ex)
+li $v0, 1
+li $a0, 5
+syscall
+
+Prints 5
+"""
     pass
 
+# Defines what #assign does
 def ASSIGN():
     pass
 
+# Defines what #infix does
 def INFIX():
     pass
 
+# Defines what #process does
 def PROCESS():
     pass
 
-def findGenerateMIPSCode(tree, dict_):
+def findGenerateMIPSCode(t, dict_):
+    if t.isLeaf():
+        return None # Stop iteration, return to the parent node
+    for i in t.children:
+        pass
+    # After traversing the nodes, we invoke certain functions
     pass
---
+++ proj5/read.asm
@@ -0,0 +1,7 @@
+.text
+	li $v0, 5
+	syscall
+	
+	li $a0, 391
+	li $v0, 1
+	syscall
