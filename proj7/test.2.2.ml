4b70cfce2f5dc9404b51be84ced833685073a002
karroje <karroje@miamioh.edu>
2016-04-23 21:49:33 -0400

    Adding  testing files

---
+++ proj7/test.2.2.ml
@@ -0,0 +1,10 @@
+begin
+   int n;
+   read(n);
+   int i := n;
+   while i >= 0
+   begin
+      write(i, " ");
+      i := i - 2;
+   end
+end
---
+++ proj7/test.3.2.ml
@@ -0,0 +1,18 @@
+begin
+if True then
+begin
+  if not False then
+  begin
+    if True or False then
+    begin
+      if not (False and False) then
+      begin
+        if True or False and False then
+        begin
+	   write("Here\n");
+	end
+      end
+    end
+  end
+end
+end
