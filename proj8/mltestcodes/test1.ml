cbc63a7ccf1298f80838022aa03b3df60a1d496f
Kwangju Kim <kimk3@miamioh.edu>
2016-04-24 15:52:20 -0400

    removed tester

--- proj8/mltestcodes/test1.ml
+++ proj8/mltestcodes/test1.ml
@@ -1,3 +1,9 @@
 begin
   write("In main.\n");
-  
+  write("In foo: 1\n");
+  write("Returned to main.\n");
+  write("In foo: 1\n");
+  write("Returned to main. (Again.)\n");
+  write("In foo2\n");
+  write("Returned to main.\n");
+end
