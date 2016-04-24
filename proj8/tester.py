2c9cb64512bc8c1b1366f00d622532e703d626ef
Kwangju Kim <kimk3@miamioh.edu>
2016-04-24 15:36:59 -0400

    we don't necessary need parser, so we are not using it.

--- proj8/tester.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-from subprocess import PIPE
-
-Cproc = subprocess.Popen("./tester", stdin = PIPE, stdout = PIPE, stderr = PIPE, universal_newlines = True)
-MIPSproc = subprocess.Popen(['java', '-jar', 'mars.jar', 'out.asm'], stdin = PIPE, stdout = PIPE, stderr = PIPE, universal_newlines = True)
