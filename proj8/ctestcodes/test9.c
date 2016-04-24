e10d9754856eda47f0d22b859e6a47426056fe71
Kwangju Kim <kimk3@miamioh.edu>
2016-04-24 14:43:07 -0400

    added desired output

--- proj8/ctestcodes/test9.c
+++ proj8/ctestcodes/test9.c
@@ -1,14 +1,16 @@
 // Complex pass by reference
+#include <stdio.h>
 
 int foo(int* x, int* y) {
-  x = x + 1;
-  y = y + 1;
-  return x + y;
+  *x = *x + 1;
+  *y = *y + 1;
+  return *x + *y;
 }
 
 int main() {
   int a = 0;
-  int b = foo(a);
+  int c = 3;
+  int b = foo(&a, &c);
   printf("%d\t%d\n", a, b);
 
   return 0;
